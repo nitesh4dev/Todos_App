2a9c381b48b03f6a31c2bfe6c3e490f94e54beaf
Nitesh Vishwakarma <devnitesh2022@gmail.com>
2022-09-14 00:34:46 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import React, { useEffect, useState } from "react"
 import "./App.css"
-import { MdDelete , MdModeEdit , MdAdd} from "react-icons/md";
+import { MdDelete, MdModeEdit, MdAdd } from "react-icons/md";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
   const [input, setInput] = useState('');
   const [items, setItems] = useState([]);
-  const [icon , setIcon] = useState(false);
-  const [editId , setEditId] = useState(null);
-  console.log(items)
+  const [icon, setIcon] = useState(false);
+  const [editId, setEditId] = useState(null);
 
   const getArray = () => {
     if (!input) {
+      toast.warn("KUCHH LIKH BHI DIJIYE", {
+        autoClose: 2000,
+      });
     }
-    else if(icon && input){
-         const edited = items.map((val) => {
-            if(val.id == editId){
-              return {...val , name:input};
-            }
-             return val;
-         })
-         setItems(edited);
-         setInput("");
-         setIcon(false);
+    else if (icon && input) {
+      const edited = items.map((val) => {
+        if (val.id == editId) {
+          return { ...val, name: input };
+        }
+        return val;
+      })
+      setItems(edited);
+      setInput("");
+      setIcon(false);
     }
     else {
-      const itemsArray = {id : new Date().getTime().toString() , name: input };
+      const itemsArray = { id: new Date().getTime().toString(), name: input };
       setItems([...items, itemsArray]);
       setInput("");
     }
@@ -38,26 +42,29 @@ const App = () => {
     setItems(updatedItem);
   }
 
-  const editItem = (id) =>{
+  const editItem = (id) => {
     setIcon(true);
-    const editable = items.find((value)=>{ return value.id == id})
+    const editable = items.find((value) => { return value.id == id })
     setInput(editable.name);
     setEditId(id);
   }
-  
+
   return (
     <>
       <div className="main">
         <div className="card">
+          <div>
+            <ToastContainer />
+          </div>
           <div className="input">
             <h1>ToDo's App</h1>
-            <input value={input} type="text" placeholder="Enter the Do's" onChange={(e) => { setInput(e.target.value) }} />
-            <span className="add" onClick={getArray}>{icon? <MdModeEdit/>: <MdAdd/>}</span>
+            <input value={input} type="text" placeholder="Enter the Do's" onKeyDown={(e)=>{if(e.key === "Enter"){getArray()}}} onChange={(e) => { setInput(e.target.value) }} />
+            <span className="add" onClick={getArray}>{icon ? <MdModeEdit /> : <MdAdd />}</span>
           </div>
           <div className="list">
             <ul>
               {items.map((value) => {
-                return <li  key={value.id} > <span onClick={()=>editItem(value.id)}><MdModeEdit/></span> {value.name}  <span onClick={() => deleteItem(value.id)}><MdDelete /></span></li>
+                return <li key={value.id} > <span onClick={() => editItem(value.id)}><MdModeEdit /></span> {value.name}  <span onClick={() => deleteItem(value.id)}><MdDelete /></span></li>
               })}
             </ul>
           </div>
